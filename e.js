d6286573028bd001e104e46b6ad6788018e6e023
leonardo <4553430486@estudante.sed.sc.gov.br>
2024-06-26 21:15:08 -0300

    fiz ate o 8

---
+++ e.js
@@ -0,0 +1,29 @@
+//5)
+let vetor=['maça', 'banana', 'laranja']
+vetor.push('uva')
+console.log (vetor)
+
+//6)
+vetor=['1', '2', '3', '4', '5']
+    vetor.push('numeros')
+    console.log (vetor)
+    
+//7)
+
+    let cores = ["vermelho", "verde", "azul"]
+for(let i=0; i<3; i++){
+    console.log(cores[i])
+}
+
+//8)
+var idade = 70
+    if(idade >= 18){
+     console.log ('voce e maior de idade')
+    }
+    else if (idade <18){
+    console.log ('vc e de menor')
+     }
+    else if (idade >= 60){
+    console.log ('vc e idoso')
+    }
+
