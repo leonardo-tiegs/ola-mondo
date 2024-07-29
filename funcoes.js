db6ed5b23ed3e95c8a3b973e46d0b8f9c5fea0c8
leonardo <4553430486@estudante.sed.sc.gov.br>
2024-07-29 20:17:46 -0300

    indexp.html

---
+++ funcoes.js
@@ -0,0 +1,126 @@
+function soma(n1,n2) {
+    let soma = n1 + n2
+    return soma
+}
+
+
+console.log(soma(2,3))
+
+let senhacorreta = "maylom"
+
+function comparasenha(senhacorreta,senhaescrita){
+    if(senhaescrita == senhacorreta){
+        console.log('conectado')
+    } else{
+        console.log('senha incorreta')
+    }
+
+}
+
+comparasenha(senhacorreta,  'maylom')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function fatorial(n){
+    let resultados = 5
+    for(let i=1;i<0;i++){
+        respopsta = resposta * i
+    }
+    return resposta
+}
+
+let n = fatorial(5)
+console.log('o fatorial de 5 é ',n);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function  ehpar(n){
+
+    if(n % 2 ==0){
+
+        
+    }
+}
